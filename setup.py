49378f542a9a9b0fe29bf10ff662bc0e49b6d0f4
susanfun <susan.fung.data@gmail.com>
2018-02-14 11:29:12 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='ssg_pymeans',
+    version='1.0',
+    author='Sophia Wang, Susan Fung, Guanchen Zhang',
+    packages=['ssg_pymeans'],
+    url='https://github.com/UBC-MDS/ssg_pymeans',
+    description='A Python package for k-means clustering',
+    long_description=open('README.txt').read(),
+)

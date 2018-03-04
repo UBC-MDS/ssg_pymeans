16f176f88a0f4dc90b3e8215d7ddfb50e726585b
susanfun <susan.fung.data@gmail.com>
2018-03-04 09:44:43 -0800

    Modify test scripts based on feedback from team

--- test.py
+++ test.py
@@ -28,29 +28,26 @@ class Test_init_cent:
 
 class Test_fit:
     @pytest.fixture
-    def pymeans(self):
-        return Pymeans()
+    def result(self):
+        pymeans = Pymeans()
+        return pymeans.fit()
 
-    def test_type(self,pymeans):
-        result = pymeans.fit()
+    def test_type(self, result):
         assert type(result) == "dictionary"
 
-    def test_length(self,pymeans):
-        result = pymeans.fit()
+    def test_length(self, result):
         assert len(result) == 3
 
-    def test_clustering_type(self,pymeans):
-        result = pymeans.fit()
+    def test_clustering_type(self, result):
         assert isinstance(result[clustering], pd.DataFrame)
 
-    def test_wss_type(self,pymeans):
-        result = pymeans.fit()
+    def test_wss_type(self, result):
         assert type(result[wss]) == "float"
 
-    def test_wss_positive(result,pymeans):
+    def test_wss_positive(self, result):
         assert result[wss] >= 0
 
-    def test_centriod_type(result,pymeans):
+    def test_centriod_type(self, result):
         assert isinstance(result[centroid], pd.DataFrame)
 
 class Test_predict:
